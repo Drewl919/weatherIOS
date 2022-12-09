0c760b63c39d66d6db80210916d0c35c52c1d1e0
Andrew Musielak <Drewzic919@gmail.com>
2022-12-09 13:44:14 -0600

    init

---
+++ weatherIOS/weatherIOS/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  weatherIOS
+//
+//  Created by Andrew Musielak on 12/8/22.
+//
+
+import UIKit
+import SwiftUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    
+//    @IBAction func sensor(_ sender: UIButton, forEvent event: UIEvent) {
+//        let story = UIStoryboard(name: "Main", bundle: nil)
+//        let controller = story.instantiateViewController(identifier: "sensorData")
+//        self.navigationController?.pushViewController(controller, animated: true)
+//    }
+//
+//    @IBAction func currentWeather(_ sender: UIButton, forEvent event: UIEvent) {
+//        let story = UIStoryboard(name: "Main", bundle: nil)
+//        let controller = story.instantiateViewController(identifier: "weatherData")
+//        self.navigationController?.pushViewController(controller, animated: true)
+//    }
+    
+}
+
